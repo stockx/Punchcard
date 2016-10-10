05a82268fb2cb81de2e4bdc6a2150727bf6dc9a6
Josh Sklar <jrmsklar@gmail.com>
2016-10-10 17:19:50 -0400

    enhance demo screen with option to modify state

--- Punchcard/ViewController.swift
+++ Punchcard/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var punchesRequiredTextField: UITextField!
+    @IBOutlet weak var punchesReceivedTextField: UITextField!
+    @IBOutlet weak var punchCardView: PunchcardView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,3 +27,22 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        guard let punchesRequired = Int(punchesRequiredTextField.text ?? ""),
+            let punchesReceived = Int(punchesReceivedTextField.text ?? "")
+            where punchesReceived <= punchesRequired else {
+                return false
+        }
+        
+        var state = punchCardView.state
+        state.punchesRequired = punchesRequired
+        state.punchesReceived = punchesReceived
+        state.punchNumberColor = UIColor.lightGrayColor()
+        punchCardView.state = state
+        
+        return true
+    }
+    
+}
