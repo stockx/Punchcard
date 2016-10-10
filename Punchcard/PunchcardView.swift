5938c1de1aa1691aeb73b19bcb7b92c1ea0d8bd4
Josh Sklar <jrmsklar@gmail.com>
2016-10-10 16:40:59 -0400

    add state

--- Punchcard/PunchcardView.swift
+++ Punchcard/PunchcardView.swift
@@ -10,4 +10,41 @@ import UIKit
 
 class PunchcardView: UIView {
     
+    struct State {
+        var punchesRequired: Int
+        var punchesReceived: Int
+        
+        var emptyPunchImage: UIImage?
+        var filledPunchImage: UIImage?
+        
+        var rewardText: String
+        
+        var punchNumberFont: UIFont?
+        var punchNumberColor: UIColor?
+    }
+    
+    var state: State = State(punchesRequired: 0, punchesReceived: 0, emptyPunchImage: nil, filledPunchImage: nil, rewardText: "", punchNumberFont: nil, punchNumberColor: nil) {
+        didSet {
+            update(oldValue)
+        }
+    }
+    
+    // MARK: Init
+    
+    init(state: State) {
+        super.init(frame: CGRect.zero)
+        self.state = state
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // MARK: State
+    
+    func update(oldState: State) {
+        
+    }
+    
+    
 }
