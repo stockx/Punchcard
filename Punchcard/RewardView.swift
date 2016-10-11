8215587c53027febcf6c9d5605d79d71a25d4197
Josh Sklar <jrmsklar@gmail.com>
2016-10-11 15:08:29 -0400

    add RewardView class

---
+++ Punchcard/RewardView.swift
@@ -0,0 +1,19 @@
+//
+//  RewardView.swift
+//  Punchcard
+//
+//  Created by Sklar, Josh on 10/11/16.
+//  Copyright © 2016 StockX. All rights reserved.
+//
+
+import UIKit
+
+/**
+ Represents the last item on the Punchcard.
+ 
+ Contains a circular label with text in it for the reward of filling
+ every punch in the punchcard.
+ */
+class RewardView: UIView {
+    
+}
